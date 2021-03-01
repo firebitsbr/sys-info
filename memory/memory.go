bec7fce959d947ebb0aa5eadafb7d77acf015313
xcgc <74693042+xcgc@users.noreply.github.com>
2021-03-02 01:00:08 +0800

    Create memory.go

---
+++ memory/memory.go
@@ -0,0 +1,34 @@
+package memory
+
+import (
+	"sync"
+	"syscall"
+)
+
+type Memory_t struct {
+	Free uint64
+	Total uint64
+	SwapFree uint64
+	SwapTotal uint64
+	Mu sync.Mutex
+}
+
+func Get() (*Memory_t, error) {
+	sysInfo := &syscall.Sysinfo_t{}
+	memoryInfo := &Memory_t{}
+
+	err := syscall.Sysinfo(sysInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	defer memoryInfo.Mu.Unlock()
+
+	memoryInfo.Mu.Lock()
+	memoryInfo.Free = uint64(sysInfo.Freeram)
+	memoryInfo.Total = uint64(sysInfo.Totalram)
+	memoryInfo.SwapFree = uint64(sysInfo.Freeswap)
+	memoryInfo.SwapTotal = uint64(sysInfo.Totalswap)
+
+	return memoryInfo, nil
+}
